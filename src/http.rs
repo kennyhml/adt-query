97c89cca98706c859233d60e69af6d2c09c1b0d7
Kenny <106347478+kennyhml@users.noreply.github.com>
2025-08-06 20:49:22 +0200

    refactor(http): Abstract over (de)serialization

--- src/http.rs
+++ src/http.rs
@@ -1,15 +1,17 @@
-use http::{Request, Response};
-use std::{collections::HashMap, time::Duration};
-
 use async_trait::async_trait;
-use derive_builder::Builder;
-
-use crate::auth::AuthorizationKind;
+use http::{Request, Response};
+use serde::{Deserialize, Serialize, de::DeserializeOwned};
 
 /// The interface that any HTTP Client must implement for use in the ADT Client.
 #[async_trait]
 pub trait HTTPClient: Send + Sync {
-    async fn get<T: Send>(&self, options: Request<T>) -> Response<T>;
+    async fn get<T, R>(&self, request: &Request<T>) -> Response<R>
+    where
+        T: Send + Sync + Serialize,
+        R: Send + Sync + DeserializeOwned;
 
-    async fn post<T: Send>(&self, options: Request<T>) -> Response<T>;
+    async fn post<T, R>(&self, request: &Request<T>) -> Response<R>
+    where
+        T: Send + Sync + Serialize,
+        R: Send + Sync + DeserializeOwned;
 }
