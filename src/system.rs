9e0ea9acd6b4d663e0cff9fc60585885c4a0ca18
Kenny <106347478+kennyhml@users.noreply.github.com>
2025-08-06 20:49:49 +0200

    feat(system): Initial session

--- src/system.rs
+++ src/system.rs
@@ -1,19 +1,41 @@
+use chrono::{DateTime, Utc};
 use derive_builder::Builder;
 use http::Uri;
 
-#[derive(Builder)]
-pub struct System {
+/// Contains the information of a SAP System required to connect to the ADT Services.
+#[derive(Builder, Debug)]
+pub struct ConnectionConfiguration {
+    /// The URL of the server, e.g https://my-sap-system.com:8000
+    #[builder(setter(into))]
     server_url: Uri,
+
+    /// Optional, the message server (load balancer) to use
     #[builder(default = None)]
     message_server: Option<String>,
+
+    /// The SAP Router to use, required for connection to SAP GUI.
     #[builder(default = None)]
     sap_router: Option<String>,
-}
 
-#[derive(Builder)]
-pub struct ConnectionConfig {
-    credentials: Credentials,
+    // The client to connect on
     client: i32,
+
+    // The language to connect with, e.g 'EN', 'DE'..
     #[builder(setter(into))]
     language: String,
+}
+
+pub struct StatefulSession {
+    start: DateTime<Utc>,
+    session_id: String,
+}
+
+pub struct StatelessSession {
+    start: DateTime<Utc>,
+    session_id: String,
+}
+
+pub enum Session {
+    Stateful(StatefulSession),
+    StatelessSession(StatelessSession),
 }
