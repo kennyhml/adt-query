913b757d4c8957b0418d5e507eaf7fce13e58d51
Kenny <106347478+kennyhml@users.noreply.github.com>
2025-08-21 18:37:42 +0200

    feat(checkruns): add check object endpoint

--- src/adt/checkruns.rs
+++ src/adt/checkruns.rs
@@ -1,3 +1,5 @@
+use std::borrow::Cow;
+
 use serde::{Deserialize, Serialize};
 
 use crate::api::{Endpoint, Stateless};
@@ -18,11 +20,44 @@ pub struct CheckReporters {
     reporters: Vec<Reporter>,
 }
 
+#[derive(Debug, Serialize)]
+#[serde(rename = "chkrun:checkObjectList")]
+pub struct CheckObjectList {
+    #[serde(rename = "chkrun:checkObject")]
+    objects: Vec<CheckObject>,
+}
+
+#[derive(Debug, Serialize)]
+pub struct CheckObject {
+    #[serde(rename = "@adtcore:uri")]
+    object_uri: String,
+
+    #[serde(rename = "@chkrun:version")]
+    version: String,
+}
+
+pub struct RunCheck {
+    object: String,
+    reporter: String,
+}
+
+impl Endpoint for RunCheck {
+    type RequestBody = ();
+    type ResponseBody = ();
+    type Kind = Stateless;
+
+    const METHOD: http::Method = http::Method::POST;
+
+    fn url(&self) -> Cow<'static, str> {
+        Cow::Owned(format!("sap/bc/adt/checkruns?reporters={}", self.reporter))
+    }
+}
+
 pub struct Reporters {}
 
 impl Endpoint for Reporters {
     type RequestBody = ();
-    type ResponseBody = ();
+    type ResponseBody = CheckReporters;
     type Kind = Stateless;
 
     const METHOD: http::Method = http::Method::GET;
@@ -62,5 +97,24 @@ mod tests {
 
         let result: CheckReporters = serde_xml_rs::from_str(plain_text).unwrap();
         assert_eq!(result.reporters.len(), 4, "Did not deserialize 4 reporters");
+    }
+
+    #[test]
+    fn serialize_check_objects() {
+        let config = serde_xml_rs::SerdeXml::new()
+            .namespace("chkrun", "http://www.sap.com/adt/checkrun")
+            .namespace("adtcore", "http://www.sap.com/adt/core");
+
+        let expected_result = r#"<?xml version="1.0" encoding="UTF-8"?><chkrun:checkObjectList xmlns:adtcore="http://www.sap.com/adt/core" xmlns:chkrun="http://www.sap.com/adt/checkrun"><chkrun:checkObject adtcore:uri="/sap/bc/adt/programs/programs/zwegwerf1" chkrun:version="active" /></chkrun:checkObjectList>"#;
+
+        let content = CheckObjectList {
+            objects: vec![CheckObject {
+                object_uri: String::from("/sap/bc/adt/programs/programs/zwegwerf1"),
+                version: String::from("active"),
+            }],
+        };
+
+        let result: String = config.to_string(&content).unwrap();
+        assert_eq!(result, expected_result);
     }
 }
